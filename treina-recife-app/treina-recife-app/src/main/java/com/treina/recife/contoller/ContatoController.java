5262d79632aebf13f1e2e6f115f9ce0fbc15ce8a
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-01-12 17:47:28 -0300

    Primeira atitivade

---
+++ treina-recife-app/treina-recife-app/src/main/java/com/treina/recife/contoller/ContatoController.java
@@ -0,0 +1,25 @@
+package com.treina.recife.contoller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.treina.recife.model.Contato;
+import com.treina.recife.service.ContatoService;
+
+@RestController
+public class ContatoController {
+    @PostMapping("/contatos")
+    public Contato criarNovoContato(@RequestBody Contato contato){
+        return contatoService.save(contato);
+    }
+    @GetMapping("/contatos")
+    public List<Contato> obeterTodosContatos(){
+        return contatoService.findAll();
+    }
+    @Autowired
+    private ContatoService contatoService;
+}
